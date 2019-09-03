43a6469d24e0be62be3bdac2f4a1a2ef287f3dcf
Brendan McKenzie <hello@brendanmckenzie.com>
2019-09-03 10:24:15 +1000

    Added GTM
    Former-commit-id: ad282b83b8ce434ef1ebcd5934d8aa5305dab0e1 [formerly 52275a258533b5af0039787a6ac229899b8037ca] [formerly 643710aeb3d5ec943b61e69140cf4d73de59d382 [formerly 1b159ccb6ba9659494c07cf0dd22b367c29b0507]]
    Former-commit-id: b6794cd7ef35959e9bbfa06ef43c93c2683aaace [formerly 446be7882a64934b07fa88c9b1ee4556cd27fd31]
    Former-commit-id: b8ab64956cd0eb66a6511c9a64ec2467ddfdf2e5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import App from "next/app";
 import Head from "next/head";
+import TagManager from "react-gtm-module";
 import withApolloClient from "../lib/with-apollo-client";
 import FavIcon from "../components/FavIcon";
 
@@ -9,14 +10,16 @@ import "./fonts.scss";
 import "./style.scss";
 
 class MyApp extends App<{ apolloClient: any }> {
+  componentDidMount() {
+    TagManager.initialize({ gtmId: "GTM-TS4XLK" });
+  }
+
   render() {
     const { Component, pageProps, apolloClient } = this.props;
     return (
       <>
         <Head>
           <FavIcon />
-
-          <script src="//www.googletagmanager.com/gtm.js?id=GTM-TS4XLK" async />
         </Head>
         <ApolloProvider client={apolloClient}>
           <Component {...pageProps} />
