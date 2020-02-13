04d935900159cbba0762d67e3495a8f9e5118881
Brendan McKenzie <hello@brendanmckenzie.com>
2020-02-13 16:30:29 +1100

    Update year in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,11 @@
+import moment from "moment";
+
 const Footer: React.FC = () => (
   <footer className="section container has-text-centered">
     <ul>
       <li>
         <p className="heading">
-          Take Off Go, copyright 2019. All rights reserved. ABN 15 634 608 567
+          Take Off Go, copyright {moment().format('YYYY')}. All rights reserved. ABN 15 634 608 567
         </p>
       </li>
       <li>
