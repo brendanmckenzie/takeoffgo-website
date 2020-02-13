7c44f3410bcbc667b68a5dcb1deef691716cdbb1
Brendan McKenzie <hello@brendanmckenzie.com>
2020-02-13 15:34:33 +1100

    Fix spacing around consultant name

--- components/Quote/components/ContactInformation.tsx
+++ components/Quote/components/ContactInformation.tsx
@@ -4,8 +4,9 @@ import SectionHeader from "./SectionHeader";
 const ContactInformation = ({ data }: any) => (
   <section id="contact" className="section container">
     <SectionHeader title="Contact information" />
-    Your travel consultant is <strong>{data.consultant.name}</strong>, feel free
-    to contact {data.consultant.genderPreposition} at{" "}
+    Your travel consultant is{" "}
+    <strong>{(data.consultant.name || "").trim()}</strong>, feel free to contact{" "}
+    {data.consultant.genderPreposition} at{" "}
     <a href={`mailto:${data.consultant.email}`}>{data.consultant.email}</a> or{" "}
     <a href={`tel:${data.consultant.phone.replace(/\s/g, "")}`}>
       {data.consultant.phone}
