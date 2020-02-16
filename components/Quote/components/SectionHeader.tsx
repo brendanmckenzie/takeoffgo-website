a0560d9876e226455d5d9872a9922f38ec3cbe4b
Brendan McKenzie <hello@brendanmckenzie.com>
2020-02-16 14:42:11 +1100

    Minor tweaks

--- components/Quote/components/SectionHeader.tsx
+++ components/Quote/components/SectionHeader.tsx
@@ -4,7 +4,7 @@ import { Columns, Column } from "../../Bulma";
 const SectionHeader = ({ title, subtitle }: any) => (
   <Columns>
     <Column>
-      <h2 className="title is-3 section-header">{title}</h2>
+      <h2 className="title is-4">{title}</h2>
       {subtitle && <h4 className="subtitle is-6">{subtitle}</h4>}
     </Column>
     <Column className="is-hidden-print" narrow>
